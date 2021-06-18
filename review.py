ce526e61d15d7637e2b12d0568f72c857bcea494
days109 <larstraschen@gmail.com>
2021-06-18 23:58:55 +0800

    avg

--- review.py
+++ review.py
@@ -1,6 +1,7 @@
 # review catch
 
 data = []
+length = []
 count = 0
 with open ('reviews.txt', 'r') as f:
 	for line in f:
@@ -8,6 +9,17 @@ with open ('reviews.txt', 'r') as f:
 		count += 1 #count = count + 1
 		if count % 100000 == 0:
 			print(len(data))
+for word in data:
+	a = len(word)
+	length.append(a)
+print(length[0])
+print(length[1])
+
+s = 0
+for a in data:
+	s += len(a)  #sum = sum + len(a)
+print( s / len(data))
+
 
 
 
